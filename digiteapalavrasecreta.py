b59263936ff98e1214014e00b4901264397451ac
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-10 20:39:50 -0300

    sistemaPalavraSecreta

---
+++ digiteapalavrasecreta.py
@@ -0,0 +1,11 @@
+pa ="PYTHON"
+v = 0
+p = str(input('Qual a palavra secreta ?'))
+while pa!=p:
+    p = str(input('Qual a palavra secreta ?'))
+    v = v+1
+print('Você tentou {}'.format(v))
+    
+    
+            
+        
