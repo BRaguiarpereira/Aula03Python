b40ebe649659b437d3e2ff6ab3967e7eb26e850e
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-10 20:08:24 -0300

    contagemRegressiva

---
+++ lacodeRepeticao.py
@@ -0,0 +1,25 @@
+'''
+Estrutura de repetiçao
+repetem um conjunto de instruçoes um numero finito de vezes
+
+Dois tipos:
+
+Definidos:o numero de
+repetiçao é conhecido quando se inicia o ciclo de repetiçoes
+
+Indefinidos: o número de repetiçoes nao é conhecido.
+
+Comnado while {
+Repete um conjunto de instruçoes enquanto uma condiçao for verdadeira;
+(fluxograma)
+}
+Sintaxe:
+while condicao:
+'''
+
+
+n = int(input('Digite o numero:'))
+while n!=0:
+    n= n-1
+    print(n)
+
