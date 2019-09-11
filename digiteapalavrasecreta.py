e44917cfa3ee01238a6f99c1d22ba6584af5191e
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-10 21:04:39 -0300

    outraForma
    Utilizando qualquer valor para poder executar o while.

--- digiteapalavrasecreta.py
+++ digiteapalavrasecreta.py
@@ -1,10 +1,18 @@
-pa ="PYTHON"
-v = 0
+'''
+pa ="PY"
+v = 1
 p = str(input('Qual a palavra secreta ?'))
 while pa!=p:
     p = str(input('Qual a palavra secreta ?'))
     v = v+1
 print('Você tentou {}'.format(v))
+'''
+senha = 'sdjhwjhr'
+tent = 0
+while senha !='CC':
+    senha = input('Digite a palavra:')
+    tent = tent+1
+
     
     
             
